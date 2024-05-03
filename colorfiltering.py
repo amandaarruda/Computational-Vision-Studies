5555bb35db22caf19c29c36e17df447cc94ada27
Amanda Arruda <66084295+amandaarruda@users.noreply.github.com>
2024-05-03 16:32:31 -0300

    Update colorfiltering.py

--- colorfiltering.py
+++ colorfiltering.py
@@ -2,29 +2,34 @@ import cv2
 import numpy as np
 from matplotlib import pyplot as plt
 
-# Caminho para a pasta
+# Caminho para a pasta onde está a imagem
 caminho_pasta = '/seu/caminho/aqui'
 arquivo = 'basegrande.jpg'
 
+# Lendo a imagem especificada no caminho definido anteriormente
 frame = cv2.imread(f"{caminho_pasta}/{arquivo}")
 
-img = frame
+# Convertendo a imagem para o espaço de cores HSV (Hue, Saturation, Value)
+hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-hsv = cv2.cvtColor(img,cv2.COLOR_BGR2HSV)
+# Definindo os intervalos de cores azul e amarelo
+lower_blue = np.array([135, 68, 32])
+upper_blue = np.array([255, 178, 127])
 
-lower_blue = np.array([147,71,20])
-upper_blue = np.array([255,178,127])
+lower_yellow = np.array([42, 132, 129])
+upper_yellow = np.array([187, 226, 231])
 
-lower_yellow = np.array([91, 147, 150])
-upper_yellow = np.array([204, 255, 251])
+# Criando máscaras para as cores azul e amarelo
+mask1 = cv2.inRange(frame, lower_blue, upper_blue)
+mask2 = cv2.inRange(frame, lower_yellow, upper_yellow)
 
-mask1 = cv2.inRange(img,lower_blue,upper_blue)
-mask2 = cv2.inRange(img,lower_yellow,upper_yellow)
+# Combinando as máscaras usando a operação bitwise OR
+mask = cv2.bitwise_or(mask1, mask2)
 
-mask = cv2.bitwise_or(mask1,mask2)
+# Aplicando a máscara combinada à imagem original
+color_image = cv2.bitwise_and(frame, frame, mask=mask)
 
-color_image = cv2.bitwise_and(img, img, mask=mask)
-
+# Exibindo a imagem resultante
 cv2.imshow('Color Image', color_image)
 cv2.waitKey(0)
 cv2.destroyAllWindows()
